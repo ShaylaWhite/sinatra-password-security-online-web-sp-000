8d6805c39dc5c407f91f396bab9807d6b24a3338
Shayla White <shayladwhite1@gmail.com>
2020-04-02 02:08:52 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -26,39 +26,44 @@ binding.pry
     end
   end
 
-	get "/login" do
-		erb :login
-	end
+	get '/account' do
+    @user = User.find(session[:user_id])
+    erb :account
+  end
 
-	post "/login" do
-		#your code here!
-	end
 
-	get "/success" do
-		if logged_in?
-			erb :success
-		else
-			redirect "/login"
-		end
-	end
+  get "/login" do
+    erb :login
+  end
 
-	get "/failure" do
-		erb :failure
-	end
+  post "/login" do
+    user = User.find_by(:username => params[:username])
+ 
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect "/account"
+    else
+      redirect "/failure"
+    end
+  end
 
-	get "/logout" do
-		session.clear
-		redirect "/"
-	end
+  get "/failure" do
+    erb :failure
+  end
 
-	helpers do
-		def logged_in?
-			!!session[:id]
-		end
+  get "/logout" do
+    session.clear
+    redirect "/"
+  end
 
-		def current_user
-			User.find(session[:id])
-		end
-	end
+  helpers do
+    def logged_in?
+      !!session[:user_id]
+    end
+
+    def current_user
+      User.find(session[:user_id])
+    end
+  end
 
 end
