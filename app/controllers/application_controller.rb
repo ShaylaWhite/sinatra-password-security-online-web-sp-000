46db38611a635eb01e62ebe467c0bd5f396df8e6
Shayla White <shayladwhite1@gmail.com>
2020-04-02 02:56:18 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,7 +16,7 @@ class ApplicationController < Sinatra::Base
     erb :signup
   end
 
-  post "/signup" do
+  post "/login" do
 	user = User.new(:username => params[:username], :password => params[:password])
 
 	  if user.save && user[:username] != ""
