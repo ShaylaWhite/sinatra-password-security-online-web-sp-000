cb801ae789fa47493ef5a2bf364ecf2b3f2f8566
Shayla White <shayladwhite1@gmail.com>
2020-04-02 03:01:20 +0000

    Automatically backed up by Learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,3 @@
-class CreateUsers < ActiveRecord::Migration[5.1]
-  def up
-    create_table :users do |t|
-      t.string :username
-      t.string :password_digest
-    end
-  end
- 
-  def down
-    drop_table :users
-  end
+class User < ActiveRecord::Base
+  has_secure_password
 end
